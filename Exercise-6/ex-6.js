faa930dcffb2d0e198ccea95b9bae2c51b6bf9e2
Júlia Gabriela Santi Acosta <juliagabriela98@gmail.com>
2021-05-20 12:22:41 -0300

    add answer of exercise 6

---
+++ Exercise-6/ex-6.js
@@ -0,0 +1,9 @@
+//OK - review
+
+const moveImage = () => {
+    const offering = document.getElementById("solncat-offering");
+    const hero = document.getElementById("solncat-hero");
+    offering.insertBefore(hero, offering.childNodes[0]);
+}
+
+moveImage();
