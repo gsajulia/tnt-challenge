820661913f0476429e74610246297f93972743ff
Júlia Gabriela Santi Acosta <juliagabriela98@gmail.com>
2021-05-20 12:20:25 -0300

    add answer of exercise 4

---
+++ Exercise-4/ex-4.js
@@ -0,0 +1,13 @@
+// Ok - review
+
+const changeBackground = () => {
+    const imageUrl = "https://placekitten.com/1280/425";
+    
+    //Desktop
+    $('#solnoff-breadcrumb').parent().css("background-image", "url(" + imageUrl + ")");
+    
+    //Mobile
+    $('.header-img').css("background-image", "url(" + imageUrl + ")");
+}
+
+changeBackground();
